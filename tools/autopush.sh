b646cd9480e97df5972b568bfe49f603becf5560
wqy88 <wqy88@163.com>
2019-10-26 15:27:51 +0800

    这是2019-10-26_15:27的维护日志，由wqy88程序自动创建

--- tools/autopush.sh
+++ tools/autopush.sh
@@ -3,11 +3,11 @@ nowtime=`date +%F_%H:%M`
 git init
 git add -A
 git commit -m "这是${nowtime}的维护日志，由wqy88程序自动创建"
-git remote rm mylink
-git remote add mylink git@gitee.com:wqy88/links.git
-#强制更新到远程 git push -f mylink master
-git push mylink master
-git remote rm link
-git remote add link git@github.com:wqy88/links.git
-git push link master 
+git remote rm myweb
+git remote add myweb git@gitee.com:wqy88/web.git
+#强制更新到远程 git push -f myweb master
+git push myweb master
+git remote rm web
+git remote add web git@github.com:wqy88/web.git
+git push web master 
 #read -p "Press [Enter] key to continue."
