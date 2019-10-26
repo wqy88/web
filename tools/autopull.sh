59acca349d653b0147ed36b45d14870df757ea31
wqy88 <wqy88@163.com>
2019-10-26 15:27:03 +0800

    这是2019-10-26_15:27的维护日志，由wqy88程序自动创建

---
+++ tools/autopull.sh
@@ -0,0 +1,8 @@
+git init
+#git remote rm myweb
+#git remote add myweb https://gitee.com/wqy88/web.git
+#git pull myweb master
+
+#git remote rm web
+git remote add web https://github.com/wqy88/web.git
+git pull web master
